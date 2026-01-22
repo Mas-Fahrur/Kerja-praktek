419034036fb41f739691d45670c5c3055f679870
Nama Kamu <emailkamu@gmail.com>
2026-01-22 15:41:14 +0700

    ganti cara baca css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resto Nusantara</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="/css/style.css">
+
 </head>
 
 <body>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Menu</title>
 
-    <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+    <link rel="stylesheet" href="/css/menu.css">
+
 
 </head>
 <body>
